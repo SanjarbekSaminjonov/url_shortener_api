70cf0b601d96dd519a1390e3529b0b7437eadffa
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2021-11-19 23:42:54 +0500

    collectstatic

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,8 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8000',
-    'http://localhost:3000',
+    # 'http://127.0.0.1:8000',
+    # 'http://127.0.0.1:3000',
+    # 'http://localhost:8000',
+    # 'http://localhost:3000',
 )
